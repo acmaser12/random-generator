3839ab512e80ef654d7bb7afd6b3c2eb04de502d
Adam Maser <acmaser12@gmail.com>
2020-01-16 19:57:55 -0600

    Added getfullNames() method, provides ability to generate full names randomly from first and last name lists.

--- src/Generator.java
+++ src/Generator.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -19,8 +21,8 @@ public class Generator {
         System.out.println("I certify that this is my own work");
 
         // declare ArrayLists to store names
-        ArrayList<String> firstNames;
-        ArrayList<String> lastNames;
+        ArrayList<String> firstNames = new ArrayList<>();
+        ArrayList<String> lastNames = new ArrayList<>();
 
         // get ArrayLists from files and store in variables
         try {
@@ -29,10 +31,12 @@ public class Generator {
         } catch (FileNotFoundException ex) {
             System.out.println("Error: File not found in project directory...");
         }
+
+        //randomly assign first names to last names
+        HashMap<String, String> fullNames = getFullNames(firstNames, lastNames);
     }
 
-
-    //loads first and last names from files and creates full names
+    // loads first and last names from files and creates full names
     public static ArrayList<String> getNames(File nameFile) throws FileNotFoundException {
         // read files into arrays
         Scanner nameReader = new Scanner(nameFile);
@@ -42,5 +46,25 @@ public class Generator {
         }
         // DEBUG System.out.println(firstNames);
         return names;
+    }
+
+    // generates pairs of first and last names
+    public static HashMap<String, String> getFullNames(ArrayList<String> firstNames, ArrayList<String> lastNames) {
+        // HashMap container
+        HashMap<String, String> fullNames = new HashMap<>();
+
+        // loop through and create full names until limit is met
+        Random r = new Random();
+        for (int i = 0; i < 20; i++) {
+            // generate random indices
+            int randomFirstName = r.nextInt(firstNames.size());
+            int randomLastName = r.nextInt(lastNames.size());
+
+            // get full name from params and store in HashMap
+            fullNames.put(lastNames.get(randomLastName), firstNames.get(randomFirstName));
+        }
+
+        // return full names
+        return fullNames;
     }
 }
